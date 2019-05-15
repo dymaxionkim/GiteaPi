a47aae8723ec7ebde8a00d5936ff7c62f37a044e
dymaxionkim <dymaxion.kim@gmail.com>
2019-05-15 15:19:16 +0900

    External Renderer

--- UpdateGitea.sh
+++ UpdateGitea.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mv -f /mnt/HDD1/GiteaPi/gitea /mnt/HDD1/GiteaPi/gitea.old
-wget -O /mnt/HDD1/GiteaPi/gitea https://dl.gitea.io/gitea/master/gitea-master-linux-arm-7
+wget -O /mnt/HDD1/GiteaPi/gitea https://dl.gitea.io/gitea/master/gitea-master-linux-arm-6
 chmod +x /mnt/HDD1/GiteaPi/gitea
 
 exit 0
