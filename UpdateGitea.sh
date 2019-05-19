697d3601540aad1afa96a041b661450c8299c758
Gitea <gitea@fake.local>
2019-05-19 04:35:41 +0000

    by ~/.GitPush_GiteaPi.sh on NUC server

--- UpdateGitea.sh
+++ UpdateGitea.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 mv -f /mnt/HDD1/GiteaPi/gitea /mnt/HDD1/GiteaPi/gitea.old
-wget -O /mnt/HDD1/GiteaPi/gitea https://dl.gitea.io/gitea/master/gitea-master-linux-arm-6
+#wget -O /mnt/HDD1/GiteaPi/gitea https://dl.gitea.io/gitea/master/gitea-master-linux-arm-6
+wget -O /mnt/HDD1/GiteaPi/gitea https://dl.gitea.io/gitea/master/gitea-master-linux-amd64
 chmod +x /mnt/HDD1/GiteaPi/gitea
 
 exit 0
-
-
